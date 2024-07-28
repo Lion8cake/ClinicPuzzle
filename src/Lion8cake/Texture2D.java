125df8df9fc3454c84525ea755b862e8fad33d92
Lion8cake <Lion8cake@hotmail.com>
2024-07-28 23:47:38 +1000

    Evicted the already done TODO in Texture2D

--- src/Lion8cake/Texture2D.java
+++ src/Lion8cake/Texture2D.java
@@ -8,7 +8,6 @@ import java.util.Hashtable;
 import javax.imageio.ImageIO;
 
 //Image System Made by Lion8cake
-//TODO: ADD DICTIONARY SO IMAGES AREN'T CONSTANTLY BEING GOTTEN FROM THE SOURCE FILES!!
 public class Texture2D {
 
 	private static String[] ValidExtentions = { ".png", ".ico" }; //reads .Ico files that are just renamed .png files because I cannot be bothered making a whole .Ico reader
